b1a8d1b7f9620374ffe78c554d9b8801bcfb9b0e
Eronita Emini <eronitaemini@gmail.com>
2024-05-28 18:33:32 +0200

    Fixed link navigation and dark mode button test, added extra tests

--- cc.spec.js
+++ cc.spec.js
@@ -6,58 +6,37 @@ test.beforeEach(async({page})=>{
     
 });
 
-test('has title GeeksforGeeks', async ({ page }) => {
+test('Has title GeeksforGeeks testing', async ({ page }) => {
     await expect(page).toHaveTitle(/GeeksforGeeks/);
   });
 
-test('Testing the search bar',async({page})=>{
+test('Search bar Testing',async({page})=>{
    await page.fill('.ant-input','Python');
    await page.click('.ant-input-group-addon');
    await page.waitForLoadState('networkidle');
 
 });
 
-test('Test dark mode button', async({page})=>{
-    await page.click('gfg-icon_dark-mode');
-    await page.waitForLoadState('networkidle');
-    const isDarkModeEnabled = await page.evaluate(() => {
-        const bodyBackgroundColor = window.getComputedStyle(document.body).backgroundColor;
-        return isDarkMode(bodyBackgroundColor);
-      });
-    
-      expect(isDarkModeEnabled).toBe(true);
-    });
-    
-    function isDarkMode(color) {
-      const rgb = color.match(/\d+/g).map(Number);
-      const brightness = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
-      return brightness <= 125;
-    }
-
-
-test('Testing article navigation', async({page})=>{
-    await page.goto('https://www.geeksforgeeks.org/data-structures/');
-    
-
-    await page.click('a[href="#advanced-data-structures"]');
-    await page.waitForTimeout(1000);
-    const isSectionInView = await page.evaluate(() => {
-        const section = document.getElementById('#advanced-data-structures');
-        const bounding = section.getBoundingClientRect();
-        return (
-          bounding.top >= 0 &&
-          bounding.left >= 0 &&
-          bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-          bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-      });
-    
-      // Assert that the specified section is in view
-      expect(isSectionInView).toBeTruthy();
-
-
+test('Dark mode button testing', async({page})=>{
+  await page.locator('.gfg-icon_dark-mode').click();
+  await expect(page.locator('.custom-background-white')).toHaveAttribute('data-dark-mode', 'true');
 });
 
 
+test('Link direction testing', async({page})=>{
 
-  
+  await page.getByRole('link', {name: 'Machine Learning Tutorial'}).click();
+  await expect(page).toHaveURL('https://www.geeksforgeeks.org/machine-learning/?ref=outindfooter');
+});
+
+test('Sign in testing', async({page})=>{
+  await page.locator('.header-main__signup').click();
+  await page.fill('#luser','eronitaemini');
+  await page.fill('#password', 'ee1234');
+  await page.locator('.signin-button').click();
+  const errorAlert=page.locator('.alert-danger');
+  await expect(errorAlert).toBeVisible();
+ 
+});
+
+
